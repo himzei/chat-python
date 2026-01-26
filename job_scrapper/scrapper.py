8c309a94d6396cea110215ea052423432d2c2565
unknown <himzei@gmail.com>
2026-01-26 19:46:03 +0900

    0126

--- job_scrapper/scrapper.py
+++ job_scrapper/scrapper.py
@@ -1,8 +1,16 @@
 import requests
 from bs4 import BeautifulSoup 
 
-response =requests.get("https://search.incruit.com/list/search.asp?col=job&kw=%B0%A3%C8%A3%BB%E7")
-# print(response.text)
+keyword = "간호사"
+response =requests.get(f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}")
+# print(f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}")
+# # print(response.text)
 
 soup = BeautifulSoup(response.text, "html.parser")
-print(soup)
+
+lis = soup.find_all("li", class_="c_col")
+
+# print(len(lis)) 
+for li in lis:
+    company = li.find("a", class_="cpname").text
+    print(company)
