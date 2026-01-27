f7657340446cb9d0cca61222e7510479dbbd0e2e
unknown <himzei@gmail.com>
2026-01-27 19:43:26 +0900

    re

---
+++ job_scrapper/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from scrapper import search_incruit 
+
+app = Flask(__name__) 
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route("/search")
+def search(): 
+
+    keyword = request.args.get("keyword")
+    jobs = search_incruit(keyword, 1)
+    return render_template("search.html", keyword = keyword, jobs = jobs)
+
+if __name__ == "__main__":
+    app.run()
--- job_scrapper/scrapper.py
+++ job_scrapper/scrapper.py
@@ -69,15 +69,6 @@ def search_incruit(keyword, page=1):
     return jobs 
 
 
-jobs = search_incruit("파이썬", 10)
-
-with open("jobs.csv", "w", newline="", encoding="cp949") as file:
-    writer = csv.writer(file)
-
-    writer.writerow(["회사이름", "공고제목", "회사위치", "자세히보기"])
-    for job in jobs: 
-        writer.writerow([job["회사이름"], job["공고제목"], job["회사위치"], job["자세히보기"]])
-
 
 
 
